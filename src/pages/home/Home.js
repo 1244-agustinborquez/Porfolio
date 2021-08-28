d1cd2ce60a9ddb320166d431b2c31f4b1c06932a
Guillermo Agustín Borquez <agustinborquez1999@hotmail.com>
2021-08-28 14:40:52 -0300

    home page finished

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,36 @@
-import React from 'react'
-
+import React from 'react';
+import './Home.css';
+import Typical from 'react-typical';
 function Home() {
     return (
         <div className='home'>
-            <h1>Soy el home</h1>
+            <div className='main-home'>
+            <div className='content-home'>
+                <h2 className='text-home'>Mi Nombres es Guillermo Agustín</h2>
+                <h3 className='text-home'> Soy Full Stack Developer 🚀
+                {/* <Typical
+                    loop={Infinity}
+                    wrapper='b'
+                    steps={[
+                        ' Full Stack Developer',
+                        1000,
+                        ' Creativo',
+                        1000,
+                        ' '
+                    ]}
+                /> */}
+                </h3>
+                <p>
+                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. 
+                Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in 
+                their exact original form,
+                accompanied by English versions from the 1914 translation by H. Rackham.
+                </p>
+            </div>
+            <div className='image-home'>
+                    <img src='/ImgHeader.jpg' alt='Image No Found' width='100%' height='100%'/>
+            </div>
+            </div>
         </div>
     )
 }
